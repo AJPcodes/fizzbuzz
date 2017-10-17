8c47da7436465638299d21f7328253143b3ec904
Allen Phelps <aphelps@aspirehealthcare.com>
2017-10-17 00:22:11 -0500

    add fizzbuzz explanation to fizzbuzz.ex

--- elixir/fizzbuzz.ex
+++ elixir/fizzbuzz.ex
@@ -1,5 +1,11 @@
 defmodule FizzBuzz do
 
+
+# Write a program that prints the numbers from 1 to 100.
+# For multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”
+
+
   def start do
     fizzbuzz(1, 100)
   end
