ed81b608bcc422a77c71b1bb407b5b7e806aec5a
Allen Phelps <aphelps@aspirehealthcare.com>
2017-10-16 23:23:44 -0500

    node fizzbuzz with params

--- node/fizzbuzz.js
+++ node/fizzbuzz.js
@@ -5,13 +5,21 @@ For numbers which are multiples of both three and five print “FizzBuzz”
 It would be more interesting if the numbers were -50 to +50.
 */
 
-const fizzbuzz = () => {
-  for (let i = 1; i <= 100; i++) {
-    let answer = ''
-    answer = (i%3 === 0) ? (answer + "Fizz") : answer
-    answer = (i%5 === 0) ? (answer + "Buzz") : answer
-    console.log(answer || i)
+const fizzbuzz = (start, end) => {
+
+  if (Number.isInteger(start) && Number.isInteger(end) && (start < end)) {
+    for (let i = start; i <= end; i++) {
+      let answer = ''
+      answer = (i % 3 === 0) ? (answer + "Fizz") : answer
+      answer = (i % 5 === 0) ? (answer + "Buzz") : answer
+      console.log(answer || i)
+    }
+  } else {
+    console.log("Invalid Input")
   }
 }
 
-fizzbuzz()
+// fizzbuzz(1, 10)
+// fizzbuzz("Bad Input")
+// fizzbuzz(10, 5)
+// fizzbuzz(-50, -25)
