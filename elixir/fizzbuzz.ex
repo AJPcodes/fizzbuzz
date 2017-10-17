38da32021d4315a56f776a2b51fe78db430bac5d
Allen Phelps <aphelps@aspirehealthcare.com>
2017-10-17 00:16:27 -0500

    elixir fizzbuzz with params

--- elixir/fizzbuzz.ex
+++ elixir/fizzbuzz.ex
@@ -1,16 +1,24 @@
 defmodule FizzBuzz do
 
   def start do
-    fizzbuzz(1)
+    fizzbuzz(1, 100)
   end
 
-  defp fizzbuzz(100) do
-    IO.puts("Buzz")
+  def start(num, end_num) when is_integer(num) and is_integer(end_num) and num < end_num do
+    fizzbuzz(num, end_num)
   end
 
-  defp fizzbuzz(num) when is_integer(num) and num < 100 do
+  def start(_, _) do
+    IO.puts("Bad Input")
+  end
+
+  defp fizzbuzz(num, num) do
     print(num)
-    fizzbuzz(num + 1)
+  end
+
+  defp fizzbuzz(num, end_num) when is_integer(num) and num < 100 do
+    print(num)
+    fizzbuzz(num + 1, end_num)
   end
 
   defp print(num) do
